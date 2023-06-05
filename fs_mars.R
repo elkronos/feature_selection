1ad10ec7b741db1b1ef3dcf98a93ea134eb6a77c
elkalytics <napoleonic_bores@proton.me>
2023-06-05 19:02:07 -0400

    Updated for categorical variables

--- fs_mars.R
+++ fs_mars.R
@@ -20,18 +20,30 @@
 #' @param number The number of folds to use for cross-validation (default: 5)
 #' @return The RMSE of the predictions on the test set
 #' @examples
-#' set.seed(123)
-#' response <- rnorm(1000)
-#' predictors <- matrix(rnorm(10000), ncol = 10)
-#' data <- data.frame(response = response, predictors = predictors)
-#' rmse <- train_mars(data)
-#' rmse
-# Load packages
-library(earth)
-library(caret)
+#' # Load iris dataset
+#' data(iris)
+#' 
+#' # Rename Sepal.Length to response
+#' names(iris)[names(iris) == "Sepal.Length"] <- "response
+#' 
+#' # Test your function
+#' result <- fs_mars(iris, p = 0.7, method = "lm")
+#' 
+#' # Print the full model results
+#' print(result$model)
+#' 
+#' # Assuming you have obtained the model object from fs_mars
+#' model <- result$model
+#' 
+#' # Get variable importance
+#' var_importance <- varImp(model)
+#' 
+#' # Print variable importance
+#' print(var_importance)
+#' 
 # Save function
 fs_mars <- function(data, p = 0.8, degree = 1:3, nprune = c(5, 10, 15),
-                       method = "earth", search = "grid", number = 5) {
+                    method = "earth", search = "grid", number = 5) {
   library(earth)
   library(caret)
   
@@ -49,14 +61,27 @@ fs_mars <- function(data, p = 0.8, degree = 1:3, nprune = c(5, 10, 15),
   
   # Train the model using grid search for hyperparameters
   set.seed(123)
-  mars_model <- train(response ~ ., data = train, method = method, trControl = ctrl,
-                      tuneGrid = hyper_params)
+  if(method == "earth") {
+    mars_model <- train(response ~ ., data = train, method = method, trControl = ctrl, 
+                        tuneGrid = hyper_params)
+  } else {
+    mars_model <- train(response ~ ., data = train, method = method, trControl = ctrl)
+  }
   
   # Make predictions on test set
   pred <- predict(mars_model, newdata = test)
   
-  # Calculate the RMSE of the predictions
-  rmse <- sqrt(mean((test$response - pred)^2))
-  
-  return(rmse)
+  # Calculate the RMSE if the response variable is numeric
+  if(is.numeric(test$response)){
+    rmse <- sqrt(mean((test$response - pred)^2))
+    return(list(model = mars_model, rmse = rmse))
+  }
+  # Calculate accuracy if the response variable is categorical (factor)
+  else if(is.factor(test$response)){
+    accuracy <- sum(test$response == pred) / length(pred)
+    return(list(model = mars_model, accuracy = accuracy))
+  }
+  else{
+    stop("Unsupported response variable type.")
+  }
 }
