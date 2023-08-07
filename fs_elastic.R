de04a72834b943b9d3adbe4fb1452745abc15ca8
elkalytics <napoleonic_bores@proton.me>
2023-08-06 19:05:53 -0500

    Updates
    Enhanced the functions by adding more features and fixing limitations.

--- fs_elastic.R
+++ fs_elastic.R
@@ -1,103 +1,113 @@
-#' Elastic Net Regression with Cross-Validation
-#'
-#' This function performs elastic net regression with cross-validation, using the glmnet package.
-#'
-#' @param data A data frame containing the response variable and predictor variables.
-#' @param formula A formula specifying the model, with the response variable on the left-hand side and the predictor variables on the right-hand side.
-#' @param alpha A numeric vector of values between 0 and 1 that specifies the mixing parameter between the L1 and L2 penalties in the elastic net model.
-#' @param trControl A trainControl object specifying the cross-validation method and number of folds.
-#' @param use_pca A logical value indicating whether principal component analysis (PCA) should be performed on the predictor variables before fitting the model.
-#'
-#' @return A list with the following elements:
-#' \describe{
-#'   \item{coef}{A vector of coefficients for the best model.}
-#'   \item{alpha}{The mixing parameter between the L1 and L2 penalties for the best model.}
-#'   \item{lambda}{The regularization parameter for the best model.}
-#' }
-#'
-#' @examples
-#' # Set the seed for reproducibility
-#' set.seed(123)
-#'
-#' # Generate 100 observations and 10 predictors
-#' n <- 100
-#' p <- 10
-#' x <- matrix(rnorm(n * p), nrow = n)
-#' colnames(x) <- paste0("x", 1:p)
-#'
-#' # Generate the response variable with some noise
-#' beta <- c(1.5, -2, 0, 0, 0.8, 0, -1.2, 0.5, 0.3, -1)
-#' y <- x %*% beta + rnorm(n)
-#'
-#' # Combine the data into a data frame
-#' data <- data.frame(y, x)
-#'
-#' # Use elastic net with 5-fold cross-validation and y as the response variable
-#' elastic_select(data, y ~ .,
-#'                trControl = trainControl(method = "cv", number = 5))
-#'
-#' @importFrom glmnet glmnet
-#' @importFrom caret trainControl
-#' @importFrom doParallel makeCluster registerDoParallel stopCluster
-#' @importFrom stats prcomp model.matrix model.response
-#' @export
 # Load the necessary packages
 library(glmnet)
 library(caret)
 library(doParallel)
-fs_elastic <- function(data, formula, alpha = seq(0, 1, by = 0.1), trControl = trainControl(method = "cv", number = 5), use_pca = FALSE) {
+
+#' Elastic Net Regression with Optional PCA and Cross-Validation
+#'
+#' This function performs elastic net regression with optional PCA and cross-validation, 
+#' using the `glmnet` and `caret` packages. Parallel processing is utilized with the `doParallel` package.
+#'
+#' @param data A data frame containing the response variable and predictor variables.
+#' @param formula A formula specifying the model, with the response variable on the left-hand side 
+#' and the predictor variables on the right-hand side.
+#' @param alpha A numeric vector of values between 0 and 1 that specifies the mixing parameter 
+#' between the L1 and L2 penalties in the elastic net model. Default is a sequence from 0 to 1 in increments of 0.1.
+#' @param lambda_seq A numeric vector of lambda values for regularization. If NULL (default), 
+#' it uses `10^seq(-3, 3, length = 100)`.
+#' @param trControl A `trainControl` object specifying the cross-validation method and number of folds. 
+#' Default is 5-fold cross-validation.
+#' @param use_pca A logical value indicating whether principal component analysis (PCA) should be 
+#' performed on the predictor variables before fitting the model. Default is FALSE.
+#' @param nPCs Numeric. Number of principal components to retain if PCA is used. If NULL and PCA is used, 
+#' all components are retained.
+#'
+#' @return A list with the following elements:
+#'   \itemize{
+#'     \item \code{coef}: Coefficients of the best model.
+#'     \item \code{alpha}: The mixing parameter between the L1 and L2 penalties for the best model.
+#'     \item \code{lambda}: The regularization parameter for the best model.
+#'     \item \code{RMSE}: The root mean square error (RMSE) of the best model.
+#'   }
+#'
+#' @examples
+#' # Set the seed for reproducibility
+#' set.seed(123)
+#' # Generate a dataset
+#' n <- 100
+#' p <- 10
+#' x <- matrix(rnorm(n * p), nrow = n)
+#' colnames(x) <- paste0("x", 1:p)
+#' beta <- c(1.5, -2, 0, 0, 0.8, 0, -1.2, 0.5, 0.3, -1)
+#' y <- x %*% beta + rnorm(n)
+#' data <- data.frame(y, x)
+#'
+#' # Apply the function
+#' result <- fs_elastic(data, y ~ .)
+#'
+#' @importFrom glmnet glmnet
+#' @importFrom caret trainControl train
+#' @importFrom doParallel makeCluster registerDoParallel stopCluster detectCores
+#' @importFrom stats prcomp model.matrix model.response
+#' @export
+fs_elastic <- function(data, formula, alpha = seq(0, 1, by = 0.1), 
+                       trControl = trainControl(method = "cv", number = 5), 
+                       use_pca = FALSE, nPCs = NULL, lambda_seq = NULL) {
   
   # Extract the response and predictor variables from the formula
   y <- model.response(model.frame(formula, data))
   x <- model.matrix(formula, data)[,-1]
   
-  # Handle missing values if necessary
+  # Handle missing values
   complete_cases <- complete.cases(x, y)
-  y <- y[complete_cases]
-  x <- x[complete_cases, ]
+  if(any(!complete_cases)){
+    y <- y[complete_cases]
+    x <- x[complete_cases, ]
+  }
   
-  # Define the grid of lambda values to search over
-  lambda <- 10^seq(-3, 3, length = 100)
+  if(is.null(lambda_seq)){
+    lambda_seq <- 10^seq(-3, 3, length = 100)
+  }
   
-  # Use doParallel to run the cross-validation and grid search in parallel
+  tuneGrid <- expand.grid(alpha = alpha, lambda = lambda_seq)
   cl <- makeCluster(detectCores() - 1)
   registerDoParallel(cl)
   
-  # Load necessary libraries on each core
   clusterEvalQ(cl, {
     library(caret)
     library(glmnet)
   })
   
-  # Fit the elastic net models using cross-validation and grid search
-  cv_fit <- foreach(a = alpha, .combine = "c", .packages = c("caret", "glmnet")) %dopar% {
-    # Perform PCA if use_pca is TRUE
+  cv_fit <- foreach(i = 1:nrow(tuneGrid), .combine = "list", .packages = c("caret", "glmnet")) %dopar% {
+    a <- tuneGrid$alpha[i]
+    l <- tuneGrid$lambda[i]
+    x_train <- x
     if (use_pca) {
       pca <- prcomp(x, scale. = TRUE)
-      x_pca <- pca$x[, 1:3]
-    } else {
-      x_pca <- x
+      if(is.null(nPCs)){
+        x_train <- pca$x
+      } else {
+        x_train <- pca$x[, 1:min(nPCs, ncol(pca$x))]
+      }
     }
-    
-    # Create a tuneGrid for each alpha
-    tuneGrid <- expand.grid(alpha = a, lambda = lambda)
-    
-    fit <- train(x_pca, y,
+    fit <- train(x_train, y,
                  method = "glmnet",
-                 tuneGrid = tuneGrid,
+                 tuneGrid = expand.grid(alpha = a, lambda = l),
                  trControl = trControl)
-    
-    list(RMSE = fit$results$RMSE[which.min(fit$results$RMSE)], model = fit)
+    return(list(RMSE = min(fit$results$RMSE, na.rm = TRUE), model = fit))
   }
   
-  # Stop the parallelization
   stopCluster(cl)
   
-  # Get the model with the best performance
-  best_model <- cv_fit[[which.min(sapply(cv_fit, function(cv) cv$RMSE))]]$model
+  rmse_values <- sapply(cv_fit, function(fit) {
+    return(fit$RMSE)
+  })
   
-  # Return the coefficients and alpha and lambda values of the best model
+  best_index <- which.min(rmse_values)
+  best_model <- cv_fit[[best_index]]$model
+  
   list(coef = coef(best_model$finalModel, s = best_model$bestTune$lambda),
        alpha = best_model$bestTune$alpha,
-       lambda = best_model$bestTune$lambda)
+       lambda = best_model$bestTune$lambda,
+       RMSE = min(best_model$results$RMSE, na.rm = TRUE))
 }
--- fs_randomforest.R
+++ fs_randomforest.R
@@ -1,3 +1,4 @@
+# Load packages
 library(randomForest)
 library(doParallel)
 library(foreach)
@@ -6,25 +7,31 @@ library(caret)
 
 #' Random forest prediction function
 #'
-#' This function applies the random forest algorithm for a classification or regression task. 
-#' It has options for specifying the number of trees, whether to compute importance,
-#' feature selection, data preprocessing, and parallel computing.
+#' This function applies the random forest algorithm for either a classification or regression task. 
+#' Users can specify the number of trees, whether to compute feature importance, apply custom feature 
+#' selection, preprocess the data, and use parallel computing.
 #'
 #' @param data A data frame with columns for features and a column for the target variable.
-#' @param target A string indicating the name of the column in the \code{data} that represents the target variable.
-#' @param type A string indicating the type of the prediction task. Should be "classification" or "regression".
-#' @param ntree An integer indicating the number of trees to grow in the random forest.
-#' @param importance A logical value indicating whether importance of features should be computed.
-#' @param sample_size An integer indicating the number of observations to sample from \code{data}.
-#' @param feature_select A function that takes a data frame as input and returns a data frame with selected features.
-#' @param preprocess A function that takes a data frame as input and returns a processed data frame.
-#' @param n_cores An integer indicating the number of cores to use for parallel computing.
+#' @param target A character string indicating the name of the column in \code{data} that represents the target variable.
+#' @param type A character string indicating the type of prediction task. Should be either "classification" or "regression".
+#' @param ntree An integer indicating the number of trees to grow in the random forest. Default is 500.
+#' @param importance A logical value indicating whether to compute the importance of features. Default is TRUE.
+#' @param sample_size An optional integer indicating the number of observations to randomly sample from \code{data}.
+#' @param feature_select An optional function that takes a data frame as input and returns a data frame with selected features.
+#' @param preprocess An optional function that takes a data frame as input and returns a processed data frame.
+#' @param n_cores An integer indicating the number of cores to use for parallel computing. Default is 1 (no parallelization).
+#' @param split_ratio A numeric value between 0 and 1 indicating the proportion of data to include in the training set. Default is 0.75.
+#' @param seed An optional integer for reproducibility.
 #'
-#' @return A list with elements 'model' (the trained random forest model), 
-#' 'predictions' (a vector of predictions for the target variable in the test set), and
-#' 'accuracy' (the model's classification accuracy for classification tasks, or RMSE for regression tasks).
+#' @return A list with the following components:
+#' \itemize{
+#' \item \code{model}: The trained random forest model.
+#' \item \code{predictions}: A vector of predictions for the target variable in the test set.
+#' \item \code{accuracy}: If type is "classification", this returns the model's classification accuracy. If type is "regression", this returns the Root Mean Squared Error (RMSE).
+#' }
 #'
 #' @examples
+#' \dontrun{
 #' data(iris)
 #' iris_result <- fs_randomforest(data = iris, target = "Species", type = "classification")
 #' print(iris_result$accuracy)
@@ -32,24 +39,24 @@ library(caret)
 #'
 #' data(mtcars)
 #' mtcars_result <- fs_randomforest(data = mtcars, target = "mpg", type = "regression")
-#' print(mtcars_result$accuracy)
+#' print(mtcars_result$RMSE)
 #' print(head(mtcars_result$predictions))
+#' }
 #'
 #' @export
 fs_randomforest <- function(data, target, type, ntree = 500, importance = TRUE, 
                             sample_size = NULL, feature_select = NULL, 
-                            preprocess = NULL, n_cores = 1, split_ratio = 0.75) {
+                            preprocess = NULL, n_cores = 1, split_ratio = 0.75, seed = NULL) {
   
-  # Check required packages and load them
-  required_packages <- c("randomForest", "doParallel", "foreach", "dplyr", "caret")
-  new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
-  if(length(new_packages)) install.packages(new_packages)
-  lapply(required_packages, require, character.only = TRUE)
+  # Set seed for reproducibility
+  if(!is.null(seed)) set.seed(seed)
   
-  # Check if target exists in the data
-  if (!target %in% names(data)) {
-    stop("The target variable does not exist in the dataset")
-  }
+  # Early validations
+  if (!target %in% names(data)) stop("The target variable does not exist in the dataset")
+  if(!(type %in% c("classification", "regression"))) stop("Invalid type parameter. Choose 'classification' or 'regression'")
+  if(!is.null(preprocess) && !is.function(preprocess)) stop("preprocess must be a function")
+  if(!is.null(feature_select) && !is.function(feature_select)) stop("feature_select must be a function")
+  if(n_cores > detectCores(logical = FALSE)) stop("Specified number of cores exceeds available cores.")
   
   target_idx <- match(target, names(data))
   
@@ -57,34 +64,40 @@ fs_randomforest <- function(data, target, type, ntree = 500, importance = TRUE,
     data[[target]] <- as.factor(data[[target]])
   }
   
-  # Apply preprocessing, if specified
+  # Apply preprocessing
   if(!is.null(preprocess)) {
-    if (!is.function(preprocess)) stop("preprocess must be a function")
     data <- preprocess(data)
   }
   
-  # Apply feature selection, if specified
+  # Apply feature selection
   if(!is.null(feature_select)) {
-    if (!is.function(feature_select)) stop("feature_select must be a function")
     data <- feature_select(data)
   }
   
-  # Sample data, if specified
+  # Sample data
   if(!is.null(sample_size)) {
-    data <- data %>% sample_n(sample_size)
+    if(type == "classification") {
+      data <- data %>% group_by(!!sym(target)) %>% sample_n(sample_size/nlevels(data[[target]]))
+    } else {
+      data <- data %>% sample_n(sample_size)
+    }
   }
   
-  # Split the data into training and testing
+  # Stratified split for classification tasks
   inTrain <- createDataPartition(y = data[[target]], p = split_ratio, list = FALSE)
   training <- data[inTrain,]
   testing <- data[-inTrain,]
   
-  # Use parallel computing, if specified
+  # Parallel computing
   if(n_cores > 1) {
     registerDoParallel(cores = n_cores)
     on.exit(stopImplicitCluster())
-    rf <- foreach(ntree = rep(floor(ntree/n_cores), n_cores), .combine = randomForest::combine, 
+    rf <- foreach(ntree = rep(floor(ntree/n_cores), n_cores), 
+                  .combine = randomForest::combine, 
                   .packages = c("randomForest")) %dopar% {
+                    
+                    library(randomForest)  # Load within the child process
+                    
                     randomForest(x = training[, -target_idx],
                                  y = training[[target]],
                                  ntree = ntree, importance = importance)
@@ -95,18 +108,14 @@ fs_randomforest <- function(data, target, type, ntree = 500, importance = TRUE,
                        ntree = ntree, importance = importance)
   }
   
-  
-  # Make predictions on the test data
+  # Predictions and accuracy/RMSE
   if(type == "classification") {
     predictions <- predict(rf, newdata = testing, type = "class")
     accuracy <- sum(predictions == testing[[target]]) / length(predictions)
-  } else if(type == "regression") {
-    predictions <- predict(rf, newdata = testing)
-    accuracy <- sqrt(mean((predictions - testing[[target]])^2))
+    return(list(model = rf, predictions = predictions, accuracy = accuracy))
   } else {
-    stop("Invalid type parameter. Choose 'classification' or 'regression'")
+    predictions <- predict(rf, newdata = testing)
+    RMSE <- sqrt(mean((predictions - testing[[target]])^2))
+    return(list(model = rf, predictions = predictions, RMSE = RMSE))
   }
-  
-  # Return the model, predictions and accuracy
-  list(model = rf, predictions = predictions, accuracy = accuracy)
 }
